ba1d8cde23a7536f3f3a41e744b13ac8a7dbe4cb
BoAndArrows <bocheung15@gmail.com>
2025-06-26 21:54:20 -0400

    removed regular python

--- Canada_Geese_DF.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from IPython.display import HTML, Image
-
-canada_geese_df = pd.read_csv("Bird_Banding_Data/NABBP_2023_grp_02.csv") 
-
-subspecies_df = pd.read_csv("Bird_Banding_Data/NABBP_2023_grp_03.csv")
-
-print(canada_geese_df)
